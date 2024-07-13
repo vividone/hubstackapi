7d2768d8fa5d9d3d65d9d0bb47577aa8fafab699
anosike-jayson <kayceeanosike1@gmail.com>
2024-07-13 01:12:56 +0100

    Implemented get balance on login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -133,36 +133,44 @@ export class AuthService {
 
   async loginUser(loginUserDto: LoginUser, res: any) {
     const { email, password } = loginUserDto;
-
+  
     const user = await this.userRepo.findOne({ email });
     if (!user || !(await user.comparePassword(password))) {
       throw new BadRequestException('Invalid credentials');
     }
-
+  
     const refreshToken = this.generateRefreshToken(user._id);
-    await this.userService.updateRefreshToken(
-      user._id,
-      refreshToken.refresh_token,
-    );
-
+    await this.userService.updateRefreshToken(user._id, refreshToken.refresh_token);
+  
     this.setRefreshTokenCookie(res, refreshToken.refresh_token);
-
-    const wallet = await this.walletService.getUserWallet(user._id);
-    const hasWallet = wallet ? true : false;
-
+  
+    let hasWallet = false;
+    let balance = null;
+  
+    try {
+      const wallet = await this.walletService.getUserWallet(user._id);
+      hasWallet = !!wallet; 
+      
+      if (hasWallet) {
+        balance = await this.walletService.getSubaccountBalanceOnLogin(user._id);
+      }
+    } catch (error) {
+      console.error('Error fetching wallet or balance:', error.message);
+    }
+  
     const userData = await this.userRepo.findOne(user._id, { password: false });
     const token = await this.generateToken(userData);
-
+  
     return {
       status: 'Success',
       message: 'Login successful',
       data: userData,
       hasWallet,
+      balance,
       token,
-      refreshToken,
     };
   }
-
+  
   private setRefreshTokenCookie(res: any, token: string) {
     res.cookie('refreshToken', token, {
       httpOnly: true,
