7ed4cbc61d037bae107b954d4b497d4c098e069f
anosike-jayson <kayceeanosike1@gmail.com>
2024-07-24 16:27:04 +0100

    Endpoint to get user wallet

--- src/mock-wallet/mock-wallet.controller.ts
+++ src/mock-wallet/mock-wallet.controller.ts
@@ -6,6 +6,9 @@ import {
   HttpException,
   HttpStatus,
   UseGuards,
+  Get,
+  Param,
+  NotFoundException,
 } from '@nestjs/common';
 import { CustomRequest } from 'src/configs/custom_request';
 import { CreateMockWalletDto, EstMockWalletDto } from './mock.wallet.dto';
@@ -13,6 +16,8 @@ import { MockWalletService } from './mock-wallet.service';
 import { JwtAuthGuard } from 'src/role_auth_middleware/jwt-auth.guard';
 import { RolesAuth } from 'src/role_auth_middleware/role.auth';
 import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { MockWallet } from 'src/entity';
+import { Roles } from 'src/role_auth_middleware/roles.decorator';
 
 @ApiTags('MockWallet')
 @Controller('mockwallet')
@@ -40,6 +45,26 @@ export class MockWalletController {
     } catch (error) {
       // console.error(error);
       throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @Roles('Agent', 'Individual')
+  @UseGuards(JwtAuthGuard)
+  @ApiCreatedResponse({ type: MockWallet, description: 'expected response' })
+  @ApiOperation({ summary: 'Get wallet details of a user' })
+  @Get('/:userid')
+  async getUserWallet(
+    @Param('userid') userid: string
+  ) {
+    try {
+      const wallet = await this.mockWalletService.getUserWallet(userid);
+      return wallet;
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw new NotFoundException(error.message);
+      } else {
+        throw new Error('An error occurred while retrieving the wallet');
+      }
     }
   }
 }
