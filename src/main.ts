8fb4a5ce756d761f9793c396e6c7af4a64d10982
vividone <vividvictor@gmail.com>
2024-08-05 02:17:25 +0100

    ISW Auth Issue pt2

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,18 @@ import { rateLimit } from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    rateLimit({
-      windowMs: 15 * 60 * 1000, // 15 minutes
-      max: 100, // limit each IP to 100 requests per windowMs
-    }),
-  );
+  // app.use(
+  //   rateLimit({
+  //     windowMs: 15 * 60 * 1000, // 15 minutes
+  //     max: 100, // limit each IP to 100 requests per windowMs
+  //   }),
+  // );
   app.enableCors();
   app.setGlobalPrefix('v1');
 
   // Setup Swagger Documentation
   const configSwagger = new DocumentBuilder()
-    .setTitle("Hubstak's API Documentation V1")
+    .setTitle("Hubstack's API Documentation V1")
     .setDescription(
       "This API allows developers to access and interact with Hubstack's business logic.",
     )
