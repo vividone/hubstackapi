05a1c77272b66b4663612b7a3a87a43a08eafe67
anosike-jayson <kayceeanosike1@gmail.com>
2024-08-06 14:20:35 +0100

    chore: Wallet destructuring

---
+++ src/wallet/wallet-paystack.service.ts
@@ -0,0 +1,324 @@
+import {
+    BadRequestException,
+    Injectable,
+    NotFoundException,
+} from '@nestjs/common';
+import axios from 'axios';
+import { CreateWalletDto } from './wallet.dto';
+import { WalletRepository } from 'src/entity/repositories/wallet.repo';
+import { UserRepository } from 'src/entity/repositories/user.repo';
+import { Types } from 'mongoose';
+import { TransactionService } from 'src/transactions/transaction.service';
+import {
+    InitializeWalletFunding,
+    transactionStatus,
+    transactionType,
+} from 'src/transactions/transaction.dto';
+import { handleAxiosError } from 'src/configs/handleAxiosError';
+import { WalletService } from './wallet.service';
+
+@Injectable()
+export class PaystackWalletService {
+    constructor(
+        private readonly walletRepo: WalletRepository,
+        private readonly userRepo: UserRepository,
+        private readonly walletService: WalletService,
+        private readonly transactionService: TransactionService,
+    ) { }
+
+
+    async getBanks() {
+        const baseUrl: string = process.env.PSTK_BASE_URL;
+        const secretKey: string = process.env.PSTK_SECRET_KEY;
+        try {
+            const response = await axios.get(`${baseUrl}/bank`, {
+                headers: {
+                    Authorization: `Bearer ${secretKey}`,
+                },
+            });
+
+            const banks = { data: response.data.data, message: 'Successfull' };
+
+            return banks;
+        } catch (error) {
+            handleAxiosError(
+                error,
+                'An error occurred while retrieving the bank code',
+            );
+        }
+    }
+
+    // Get code for the bank name
+
+    private async getBankCode(bankName: string) {
+        const baseUrl: string = process.env.PSTK_BASE_URL;
+        const secretKey: string = process.env.PSTK_SECRET_KEY;
+        try {
+            const response = await axios.get(`${baseUrl}/bank`, {
+                headers: {
+                    Authorization: `Bearer ${secretKey}`,
+                },
+            });
+
+            const bank = response.data.data.find((bank) => bank.name === bankName);
+            if (!bank) {
+                throw new NotFoundException('Bank not found');
+            }
+
+            return bank.code;
+        } catch (error) {
+            handleAxiosError(
+                error,
+                'An error occurred while retrieving the bank code',
+            );
+        }
+    }
+
+    async initializePaystackWalletFunding(
+        initializeWalletFunding: InitializeWalletFunding,
+        userId: string,
+    ) {
+        const baseUrl = process.env.PSTK_BASE_URL;
+        const secretKey = process.env.PSTK_SECRET_KEY;
+        const reference = this.transactionService.generateTransactionReference();
+        const data = {
+            email: initializeWalletFunding.email,
+            amount: initializeWalletFunding.amount,
+            reference: reference,
+        };
+        // console.log('data sent to paystack', initializeWalletFunding);
+        try {
+            const response = await axios.post(
+                `${baseUrl}/transaction/initialize`,
+                data,
+                {
+                    headers: {
+                        Authorization: `Bearer ${secretKey}`,
+                        'Content-Type': 'application/json',
+                    },
+                },
+            );
+
+            const { reference } = response.data.data;
+            const { status } = response.data;
+
+            if (status === true) {
+                const transactionData = {
+                    transactionReference: reference,
+                    amount: initializeWalletFunding.amount,
+                    transactionType: transactionType.WalletFunding,
+                    transactionStatus: transactionStatus.Pending,
+                    paymentMode: initializeWalletFunding.paymentMode,
+                    transactionDetails: initializeWalletFunding,
+                    user: userId,
+                };
+
+                const startTransaction =
+                    await this.transactionService.createTransaction(transactionData);
+                return startTransaction;
+            }
+        } catch (error) {
+            handleAxiosError(error, 'Error with trnsaction creation ');
+        }
+    }
+
+    async createCustomerWallet(data: CreateWalletDto, id: string) {
+        const baseUrl: string = process.env.PSTK_BASE_URL;
+        const secretKey: string = process.env.PSTK_SECRET_KEY;
+        const { bvn, existingAccountNumber, existingBankName } = data;
+
+        try {
+            const user = await this.userRepo.findOne({ _id: id });
+
+            if (!user) {
+                throw new NotFoundException('User not found');
+            }
+
+            const email: string = user.email;
+            const first_name: string = user.firstname;
+            const last_name: string = user.lastname;
+            const phone: string = user.mobilenumber;
+
+            const customer = await this.createCustomer(
+                user._id,
+                baseUrl,
+                secretKey,
+                email,
+                first_name,
+                last_name,
+                phone,
+            );
+            const bankCode = await this.getBankCode(existingBankName);
+            // console.log('Bank Code', bankCode);
+            // Validate customer
+            const validatePayload = {
+                country: 'NG',
+                bvn: bvn,
+                bank_code: bankCode,
+                account_number: existingAccountNumber,
+            };
+
+            const validateCustomer = validatePayload; // Mock Validation
+
+            if (!validateCustomer) {
+                throw new BadRequestException('Could not validate Customer');
+            }
+
+            //TODO: SET USER KYC TO TRUE AFTER VALIDATION
+            if (process.env.ENV === 'development') {
+                const newAccountNumber = this.walletService.generateAccountNumber();
+                const dvaData = this.createDvaData(
+                    customer.customer_id,
+                    newAccountNumber,
+                );
+                await this.walletRepo.create({
+                    accountName: `HUBSTACK / ${data.firstname} ${data.lastname} `,
+                    customer_id: customer.customer_id,
+                    customerCode: customer.customer_code,
+                    accountNumber: dvaData.accountNumber,
+                    bankName: dvaData.preferred_bank,
+                    country: validatePayload.country,
+                    user: user,
+                });
+
+                return {
+                    message: 'Customer and Wallet created successfully',
+                    customer,
+                    dva: dvaData,
+                };
+            } else {
+                // Paystack live account generation
+                const newAccountNumber = await this.createDVAccount(
+                    customer.customer_id,
+                );
+                const dvaData = this.createDvaData(
+                    customer.customer_id,
+                    newAccountNumber,
+                );
+
+                await this.walletRepo.create({
+                    accountName: `HUBSTACK / ${data.firstname} ${data.lastname} `,
+                    customer_id: customer.customer_id,
+                    customerCode: customer.customer_code,
+                    accountNumber: dvaData.accountNumber,
+                    bankName: dvaData.preferred_bank,
+                    country: validatePayload.country,
+                    user: user,
+                });
+
+                return {
+                    message: 'Customer and Wallet created successfully',
+                    customer,
+                    dva: dvaData,
+                    kycResult: validateCustomer,
+                };
+            }
+        } catch (error) {
+            handleAxiosError(
+                error,
+                'An error occurred while creating customer and account',
+            );
+        }
+    }
+
+    private async createCustomer(
+        userId: string,
+        baseUrl: string,
+        secretKey: string,
+        email: string,
+        first_name: string,
+        last_name: string,
+        phone: string,
+    ) {
+        try {
+            const response = await axios.post(
+                `${baseUrl}/customer`,
+                { email, first_name, last_name, phone },
+                {
+                    headers: {
+                        Authorization: `Bearer ${secretKey}`,
+                        'Content-Type': 'application/json',
+                    },
+                },
+            );
+
+            const { id, customer_code, integration } = response.data.data;
+            return {
+                email,
+                customer_code,
+                customer_id: id,
+                integration,
+                user: userId,
+            };
+        } catch (error) {
+            handleAxiosError(
+                error,
+                'An error occurred while creating the customer',
+            );
+        }
+    }
+
+    private async validateCustomer(validatePayload: any) {
+        const baseUrl: string = process.env.PSTK_BASE_URL;
+        const secretKey: string = process.env.PSTK_SECRET_KEY;
+
+        // console.log('Validaate Payload: ', validatePayload);
+
+        const { account_number, bank_code } = validatePayload;
+
+        try {
+            const response = await axios.get(
+                `${baseUrl}/resolve?account_number=${account_number}&bank_code=${bank_code}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${secretKey}`,
+                    },
+                },
+            );
+
+            const resolves = response.data.data;
+            if (!resolves) {
+                throw new NotFoundException('Not found');
+            }
+            // console.log(response);
+
+            return resolves;
+        } catch (error) {
+            handleAxiosError(
+                error,
+                'An error occurred while retrieving the bank details',
+            );
+        }
+    }
+
+
+    private createDvaData(customerId: string, accountNumber: string) {
+        return {
+            customer: customerId,
+            preferred_bank: 'mock-bank',
+            accountNumber,
+        };
+    }
+    private async createDVAccount(customerId: string) {
+        const baseUrl: string = process.env.PSTK_BASE_URL;
+        const secretKey: string = process.env.PSTK_SECRET_KEY;
+        const dvaPayload = {
+            customer: customerId,
+            preferred_bank: 'wema-bank',
+        };
+
+        const dvaResponse = await axios.post(
+            `${baseUrl}/dedicated_account`,
+            dvaPayload,
+            {
+                headers: {
+                    Authorization: `Bearer ${secretKey}`,
+                    'Content-Type': 'application/json',
+                },
+            },
+        );
+
+        return dvaResponse.data;
+    }
+
+}
--- src/wallet/wallet.service.ts
+++ src/wallet/wallet.service.ts
@@ -22,7 +22,6 @@ import {
 export class WalletService {
   constructor(
     private readonly walletRepo: WalletRepository,
-    private readonly userRepo: UserRepository,
     private readonly transactionService: TransactionService,
     private readonly transactionRepo: TransactionRepository,
   ) {}
@@ -206,153 +205,10 @@ export class WalletService {
 
   // Get list of Nigerian Banks
 
-  async getBanks() {
-    const baseUrl: string = process.env.PSTK_BASE_URL;
-    const secretKey: string = process.env.PSTK_SECRET_KEY;
-    try {
-      const response = await axios.get(`${baseUrl}/bank`, {
-        headers: {
-          Authorization: `Bearer ${secretKey}`,
-        },
-      });
-
-      const banks = { data: response.data.data, message: 'Successfull' };
-
-      return banks;
-    } catch (error) {
-      this.handleAxiosError(
-        error,
-        'An error occurred while retrieving the bank code',
-      );
-    }
-  }
-
-  // Get code for the bank name
-
-  private async getBankCode(bankName: string) {
-    const baseUrl: string = process.env.PSTK_BASE_URL;
-    const secretKey: string = process.env.PSTK_SECRET_KEY;
-    try {
-      const response = await axios.get(`${baseUrl}/bank`, {
-        headers: {
-          Authorization: `Bearer ${secretKey}`,
-        },
-      });
-
-      const bank = response.data.data.find((bank) => bank.name === bankName);
-      if (!bank) {
-        throw new NotFoundException('Bank not found');
-      }
-
-      return bank.code;
-    } catch (error) {
-      this.handleAxiosError(
-        error,
-        'An error occurred while retrieving the bank code',
-      );
-    }
-  }
+  
 
   // Validate customer
 
-  async createCustomerWallet(data: CreateWalletDto, id: string) {
-    const baseUrl: string = process.env.PSTK_BASE_URL;
-    const secretKey: string = process.env.PSTK_SECRET_KEY;
-    const { bvn, existingAccountNumber, existingBankName } = data;
-
-    try {
-      const user = await this.userRepo.findOne({ _id: id });
-
-      if (!user) {
-        throw new NotFoundException('User not found');
-      }
-
-      const email: string = user.email;
-      const first_name: string = user.firstname;
-      const last_name: string = user.lastname;
-      const phone: string = user.mobilenumber;
-
-      const customer = await this.createCustomer(
-        user._id,
-        baseUrl,
-        secretKey,
-        email,
-        first_name,
-        last_name,
-        phone,
-      );
-      const bankCode = await this.getBankCode(existingBankName);
-      // console.log('Bank Code', bankCode);
-      // Validate customer
-      const validatePayload = {
-        country: 'NG',
-        bvn: bvn,
-        bank_code: bankCode,
-        account_number: existingAccountNumber,
-      };
-
-      const validateCustomer = validatePayload; // Mock Validation
-
-      if (!validateCustomer) {
-        throw new BadRequestException('Could not validate Customer');
-      }
-
-      //TODO: SET USER KYC TO TRUE AFTER VALIDATION
-      if (process.env.ENV === 'development') {
-        const newAccountNumber = this.generateAccountNumber();
-        const dvaData = this.createDvaData(
-          customer.customer_id,
-          newAccountNumber,
-        );
-        await this.walletRepo.create({
-          accountName: `HUBSTACK / ${data.firstname} ${data.lastname} `,
-          customer_id: customer.customer_id,
-          customerCode: customer.customer_code,
-          accountNumber: dvaData.accountNumber,
-          bankName: dvaData.preferred_bank,
-          country: validatePayload.country,
-          user: user,
-        });
-
-        return {
-          message: 'Customer and Wallet created successfully',
-          customer,
-          dva: dvaData,
-        };
-      } else {
-        // Paystack live account generation
-        const newAccountNumber = await this.createDVAccount(
-          customer.customer_id,
-        );
-        const dvaData = this.createDvaData(
-          customer.customer_id,
-          newAccountNumber,
-        );
-
-        await this.walletRepo.create({
-          accountName: `HUBSTACK / ${data.firstname} ${data.lastname} `,
-          customer_id: customer.customer_id,
-          customerCode: customer.customer_code,
-          accountNumber: dvaData.accountNumber,
-          bankName: dvaData.preferred_bank,
-          country: validatePayload.country,
-          user: user,
-        });
-
-        return {
-          message: 'Customer and Wallet created successfully',
-          customer,
-          dva: dvaData,
-          kycResult: validateCustomer,
-        };
-      }
-    } catch (error) {
-      this.handleAxiosError(
-        error,
-        'An error occurred while creating customer and account',
-      );
-    }
-  }
 
   async getUserWalletBalance(userId: string) {
     if (!Types.ObjectId.isValid(userId)) {
@@ -404,7 +260,7 @@ export class WalletService {
         throw new NotFoundException('Transaction not found.');
       }
 
-      const { amount, transactionReference } = transaction;
+      const { transactionReference } = transaction;
       const verifyPayment =
         await this.transactionService.verifyPayment(transactionReference);
       const { status } = verifyPayment;
@@ -439,241 +295,9 @@ export class WalletService {
       }
     }
   }
-  async initializePaystackWalletFunding(
-    initializeWalletFunding: InitializeWalletFunding,
-    userId: string,
-  ) {
-    const baseUrl = process.env.PSTK_BASE_URL;
-    const secretKey = process.env.PSTK_SECRET_KEY;
-    const reference = this.transactionService.generateTransactionReference();
-    const data = {
-      email: initializeWalletFunding.email,
-      amount: initializeWalletFunding.amount,
-      reference: reference,
-    };
-    // console.log('data sent to paystack', initializeWalletFunding);
-    try {
-      const response = await axios.post(
-        `${baseUrl}/transaction/initialize`,
-        data,
-        {
-          headers: {
-            Authorization: `Bearer ${secretKey}`,
-            'Content-Type': 'application/json',
-          },
-        },
-      );
 
-      const { reference } = response.data.data;
-      const { status } = response.data;
-
-      if (status === true) {
-        const transactionData = {
-          transactionReference: reference,
-          amount: initializeWalletFunding.amount,
-          transactionType: transactionType.WalletFunding,
-          transactionStatus: transactionStatus.Pending,
-          paymentMode: initializeWalletFunding.paymentMode,
-          transactionDetails: initializeWalletFunding,
-          user: userId,
-        };
-
-        const startTransaction =
-          await this.transactionService.createTransaction(transactionData);
-        return startTransaction;
-      }
-    } catch (error) {
-      this.handleAxiosError(error, 'Error with trnsaction creation ');
-    }
-  }
-
-  private async createCustomer(
-    userId: string,
-    baseUrl: string,
-    secretKey: string,
-    email: string,
-    first_name: string,
-    last_name: string,
-    phone: string,
-  ) {
-    try {
-      const response = await axios.post(
-        `${baseUrl}/customer`,
-        { email, first_name, last_name, phone },
-        {
-          headers: {
-            Authorization: `Bearer ${secretKey}`,
-            'Content-Type': 'application/json',
-          },
-        },
-      );
-
-      const { id, customer_code, integration } = response.data.data;
-      return {
-        email,
-        customer_code,
-        customer_id: id,
-        integration,
-        user: userId,
-      };
-    } catch (error) {
-      this.handleAxiosError(
-        error,
-        'An error occurred while creating the customer',
-      );
-    }
-  }
-
-  private createDvaData(customerId: string, accountNumber: string) {
-    return {
-      customer: customerId,
-      preferred_bank: 'mock-bank',
-      accountNumber,
-    };
-  }
-
-  private generateAccountNumber(): string {
+  public generateAccountNumber(): string {
     return Math.floor(1000000000 + Math.random() * 9000000000).toString();
-  }
-  private handleAxiosError(error: any, defaultMessage: string) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      const { status, statusText, data } = error.response;
-      // console.error('HTTP Error:', defaultMessage, status, statusText, data);
-      throw new BadRequestException({
-        message: defaultMessage,
-        statusCode: status,
-        statusText: statusText,
-        details: data,
-      });
-    } else if (error.request) {
-      throw new InternalServerErrorException(
-        'No response received from the server',
-      );
-    } else {
-      throw new InternalServerErrorException(defaultMessage);
-    }
-  }
-
-  private async validateCustomer(validatePayload: any) {
-    const baseUrl: string = process.env.PSTK_BASE_URL;
-    const secretKey: string = process.env.PSTK_SECRET_KEY;
-
-    // console.log('Validaate Payload: ', validatePayload);
-
-    const { account_number, bank_code } = validatePayload;
-
-    try {
-      const response = await axios.get(
-        `${baseUrl}/resolve?account_number=${account_number}&bank_code=${bank_code}`,
-        {
-          headers: {
-            Authorization: `Bearer ${secretKey}`,
-          },
-        },
-      );
-
-      const resolves = response.data.data;
-      if (!resolves) {
-        throw new NotFoundException('Not found');
-      }
-      // console.log(response);
-
-      return resolves;
-    } catch (error) {
-      this.handleAxiosError(
-        error,
-        'An error occurred while retrieving the bank details',
-      );
-    }
-  }
-
-  private async createDVAccount(customerId: string) {
-    const baseUrl: string = process.env.PSTK_BASE_URL;
-    const secretKey: string = process.env.PSTK_SECRET_KEY;
-    const dvaPayload = {
-      customer: customerId,
-      preferred_bank: 'wema-bank',
-    };
-
-    const dvaResponse = await axios.post(
-      `${baseUrl}/dedicated_account`,
-      dvaPayload,
-      {
-        headers: {
-          Authorization: `Bearer ${secretKey}`,
-          'Content-Type': 'application/json',
-        },
-      },
-    );
-
-    return dvaResponse.data;
-  }
-
-  // Create a virtual account with flutterwave
-
-  async createVirtualAccount(data: CreateWalletDto, id: string) {
-    const baseUrl: string = process.env.FLW_BASE_URL;
-    const secretKey: string = process.env.FLW_SECRET_KEY;
-    try {
-      const user = await this.userRepo.findOne({ _id: id });
-
-      if (!user) {
-        throw new NotFoundException('User not found');
-      }
-
-      const email: string = user.email;
-      const account_reference: string = this.generateAccountReference();
-      //const country = 'NG';
-
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const { existingAccountNumber, existingBankName, mobilenumber, ...rest } =
-        data;
-      const requestData = {
-        ...rest,
-        phonenumber: mobilenumber,
-        is_permanent: true,
-        tx_ref: account_reference,
-      };
-      // console.log('DATA TO FLW', requestData);
-
-      const response = await axios.post(
-        `${baseUrl}/virtual-account-numbers`,
-        requestData,
-        {
-          headers: {
-            Authorization: `Bearer ${secretKey}`,
-            'Content-Type': 'application/json',
-          },
-        },
-      );
-
-      const { account_number, account_name, bank_name } = response.data.data;
-
-      const createdVirtualAccount = {
-        email: email,
-        accountName: account_name,
-        accountNumber: account_number,
-        bankName: bank_name,
-        accountReference: account_reference,
-        user: user._id,
-      };
-
-      const savedVirtualAccount = await this.walletRepo.create(
-        createdVirtualAccount,
-      );
-
-      return {
-        message: 'Virtual Account created successfully',
-        data: savedVirtualAccount,
-      };
-    } catch (error) {
-      this.handleAxiosError(
-        error,
-        'An error occurred while creating virtual account with flutterwave',
-      );
-    }
   }
 
   private async fundWallet(userId: string, walletFundingDto: WalletFundingDto) {
@@ -953,7 +577,7 @@ export class WalletService {
   //   }
   // }
 
-  private generateAccountReference(): string {
+  public generateAccountReference(): string {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     const prefix = 'HUBSTK';
     let result = prefix;
