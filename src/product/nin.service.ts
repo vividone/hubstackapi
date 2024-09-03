1463f28aa612026dfe24ccb892def5f26083aa81
anosike-jayson <kayceeanosike1@gmail.com>
2024-09-03 15:14:35 +0100

    validate nin number

--- src/product/nin.service.ts
+++ src/product/nin.service.ts
@@ -8,19 +8,16 @@ export class NinService {
   constructor(private readonly productRepo: ProductRepository) { }
 
   async validateNIN(nin: string): Promise<any> {
-    // Define the URL (replace `{{base_url}}` with the actual base URL)
+    
     const url = `${process.env.CKID_BASE_URL}/identity/nin-direct`;
-
-    // Bearer token (use environment variables for security)
     const token = process.env.CKID_TOKEN;
 
     try {
-      // Make the POST request to the API
       const response = await axios.post(
         url,
         {
-          nin, // The NIN to validate
-          level: 1, // Include the level if needed
+          nin, 
+          level: 1, 
         },
         {
           headers: {
@@ -30,10 +27,8 @@ export class NinService {
         },
       );
 
-      // Return the response data
       return response.data;
     } catch (error) {
-      // Handle and throw any errors
       throw new Error(`Failed to validate NIN: ${error.message}`);
     }
   }
