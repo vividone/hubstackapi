f6bfa2ad7e9e28234add9e2e855b635bbbe482ce
anosike-jayson <kayceeanosike1@gmail.com>
2024-09-04 18:01:05 +0100

    refix: agent verification by admin

--- src/agent_profile/agent_profile.service.ts
+++ src/agent_profile/agent_profile.service.ts
@@ -6,12 +6,13 @@ import {
 import { AgentProfileRepository } from 'src/entity/repositories/agent_profile.repo';
 import { CreateAgentProfileDto } from './dto/agent_profile.dto';
 import { UserRepository } from 'src/entity/repositories/user.repo';
+import { Types } from 'mongoose';
 @Injectable()
 export class AgentService {
   constructor(
     private readonly agentRepo: AgentProfileRepository,
     private readonly userRepo: UserRepository,
-  ) {}
+  ) { }
 
   async updateAgentProfile(
     e_mail: string,
@@ -64,14 +65,15 @@ export class AgentService {
   }
 
   async verifyAgent(id: string) {
+    const objectId = new Types.ObjectId(id);
     const agent = await this.agentRepo.findOneAndUpdate(
-      { _id: id },
-      { $set: { agentVerified: true } }, 
+      { user: objectId },
+      { $set: { agentVerified: true } },
     );
     if (!agent) {
       throw new Error('Agent not found');
     }
     return agent;
   }
-  
+
 }
